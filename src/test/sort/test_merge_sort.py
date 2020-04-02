7ff7cedcfc19662d060bdd76ea7353c40e502099
ahmad <ahmadghorbi74@gmail.com>
2020-04-02 10:56:38 +0430

    add some tests to sort testCases

--- src/test/sort/test_merge_sort.py
+++ src/test/sort/test_merge_sort.py
@@ -10,3 +10,15 @@ class TestMergeSort(unittest.TestCase):
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 16, 20]
 
         self.assertEqual(expected, merge_sort(data))
+
+    def test_merge_2(self):
+        data = [51, 91, 98, 78, 15, 30, 41, 7, 98, 63, 58, 61, 91, 56, 86, 98, 45, 84, 1, 9]
+        expected = [1, 7, 9, 15, 30, 41, 45, 51, 56, 58, 61, 63, 78, 84, 86, 91, 91, 98, 98, 98]
+
+        self.assertEqual(expected, merge_sort(data))
+
+    def test_merge_3(self):
+        data = [25, 42, 40, 24, 1, 49, 14, 13, 28, 24, 32, 46, 0, 50, 10]
+        expected = [0, 1, 10, 13, 14, 24, 24, 25, 28, 32, 40, 42, 46, 49, 50]
+
+        self.assertEqual(expected, merge_sort(data))
